f69d20d489bd641b1a9b4397a51ad6fc456b0dd2
aqui <aquilesffoliveira@gmail.com>
2025-01-04 17:44:46 -0300

    first

---
+++ main.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    $message = "<h1>hello world</h1>";
+?>
+
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <?php echo $message; ?>
+</body>
+</html>
