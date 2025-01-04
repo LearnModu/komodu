97d6dbfedb3c9b866a47ed1f0066516f60f31c14
aqui <aquilesffoliveira@gmail.com>
2025-01-04 18:08:54 -0300

    something ig

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    // random code blah blah blah
+?>
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./static/styles.css"
+</head>
+<body>
+    <h1>Welcome to Komodu</h1>
+    <p>
+        Hosting the world's comments
+    </p>
+    <img src="static/blobs-text.png" width="50%" />
+    <h2>COMING SOON</h2>
+</body>
+</html>
